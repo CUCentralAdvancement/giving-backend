4668871445af30af9d0ff31dd5b30980c1349ae7
Alex Finnarn <alex.finnarn@gmail.com>
2021-04-06 09:30:09 -0500

    change order of initializers

--- config/initializers/zz_algoliasearch.rb
+++ config/initializers/zz_algoliasearch.rb
@@ -1,3 +1,8 @@
+put "Application ID"
+put ENV['ALGOLIASEARCH_APPLICATION_ID']
+put "API Key"
+put ENV['ALGOLIASEARCH_API_KEY']
+
 AlgoliaSearch.configuration = {
   application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'],
   api_key: ENV['ALGOLIASEARCH_API_KEY']
