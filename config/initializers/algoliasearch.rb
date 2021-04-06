e8df297df1ac799d7d47eb8c57fcc6a8ae133e94
Alex Finnarn <alex.finnarn@gmail.com>
2021-04-06 10:18:17 -0500

    remove search debugging

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,8 +1,3 @@
-puts "Application ID"
-puts ENV['ALGOLIASEARCH_APPLICATION_ID']
-puts "API Key"
-puts ENV['ALGOLIASEARCH_API_KEY']
-
 AlgoliaSearch.configuration = {
   application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'],
   api_key: ENV['ALGOLIASEARCH_API_KEY']
