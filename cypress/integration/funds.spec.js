6e6bc6fca05067944208a14716a0ef27fb46c818
Alex Finnarn <alex.finnarn@gmail.com>
2021-05-26 11:51:11 -0500

    update Fund model attributes; restrict Fund slugs to never save with license plate or write-in slugs

---
+++ cypress/integration/funds.spec.js
@@ -0,0 +1,14 @@
+describe('Fund CRUD tests', function () {
+  it('Fails to save if slug is reserved', function () {
+    cy.login('content_editor')
+
+    cy.visit('/funds/bridge-forward-scholarship-endowment/edit')
+    cy.get('#fund_slug').clear().type('write-fund')
+    cy.get('input[value="Save"]').click();
+
+    // The error should show up now since "write-fund" is a reserved slug.
+    cy.contains('Slug cannot include reserved keys: "write-fund"' +
+      ' "cu-anschutz-alumni-license-plate-fund" "cu-scholarship-license-plate-fund"' +
+      ' "cu-denver-alumni-license-plate-program"')
+  })
+})
